ac220dfa3e29b697fc2f383e8565976f271e5501
lhettmann <luk.hettmann@gmx.at>
2016-06-13 18:15:20 +0200

    Test2

--- app/src/main/java/com/example/admin/feuerwehr_bewerb/MainActivity.java
+++ app/src/main/java/com/example/admin/feuerwehr_bewerb/MainActivity.java
@@ -34,7 +34,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
-    //Projekt
+
     //Globale Variablen
     MediaPlayer mediaPlayer;
     int int_angriffsbefehl_modus;
